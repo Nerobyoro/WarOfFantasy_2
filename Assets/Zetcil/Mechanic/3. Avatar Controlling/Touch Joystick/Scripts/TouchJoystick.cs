8b0c9a2922e6134339ecc5af1f085d9805666b71
muhammad aziz <azizclone56@gmail.com>
2024-12-25 04:45:07 +0700

    WWW

--- Assets/Zetcil/Mechanic/3. Avatar Controlling/Touch Joystick/Scripts/TouchJoystick.cs
+++ Assets/Zetcil/Mechanic/3. Avatar Controlling/Touch Joystick/Scripts/TouchJoystick.cs
@@ -3,44 +3,47 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class TouchJoystick : MonoBehaviour,IPointerDownHandler,IDragHandler,IPointerUpHandler {
+public class TouchJoystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
 
-   private RectTransform container ;
-   private RectTransform handle ;
+   private RectTransform container;
+   private RectTransform handle;
 
    [Header("Input Settings")]
-   public bool ArrowKeysSimulationEnabled = false ;
+   public bool ArrowKeysSimulationEnabled = false;
 
    [Header("Joystick Value")]
    public Text ValueX;
    public Text ValueY;
    public bool ShowValue;
-   public Vector2 point ;
-   public Vector2 normalizedPoint ;
+   public Vector2 point;
+   public Vector2 normalizedPoint;
 
-    [Header("Event Settings")]
+   [Header("Event Settings")]
    public UnityEvent OnJoystickDownEvents;
    public UnityEvent OnJoystickUpEvents;
-   private float maxLength ;
-   private bool _isTouching = false ;
-   public bool IsTouching { get { return _isTouching ; } }
+   private float maxLength;
+   private bool _isTouching = false;
+   public bool IsTouching { get { return _isTouching; } }
 
-   public UnityAction OnJoystickDownAction ;
-   public UnityAction OnJoystickUpAction ;
+   public UnityAction OnJoystickDownAction;
+   public UnityAction OnJoystickUpAction;
 
-   private PointerEventData pointerEventData ;
-   private Camera cam ;
+   private PointerEventData pointerEventData;
+   private Camera cam;
 
 
 
-   private void OnEnable () {
-      OnPointerUp (null) ;
+   private void OnEnable()
+   {
+      OnPointerUp(null);
    }
 
-   private void Awake () {
-      container = transform.GetComponent <RectTransform> () ;
-      handle = container.GetChild (0).GetComponent <RectTransform> () ;
-      maxLength = (container.sizeDelta.x / 2f) - (handle.sizeDelta.x / 2f) - 5f ;
+   private void Awake()
+   {
+      container = transform.GetComponent<RectTransform>();
+      handle = container.GetChild(0).GetComponent<RectTransform>();
+      maxLength = (container.sizeDelta.x / 2f) - (handle.sizeDelta.x / 2f) - 5f;
       if (!ShowValue)
       {
          ValueX.gameObject.SetActive(false);
@@ -48,54 +51,61 @@ public class TouchJoystick : MonoBehaviour,IPointerDownHandler,IDragHandler,IPoi
       }
    }
 
-   public void OnPointerDown (PointerEventData e) {
+   public void OnPointerDown(PointerEventData e)
+   {
       if (OnJoystickDownAction != null)
-         OnJoystickDownAction.Invoke () ;
-        _isTouching = true ;
-      cam = e.pressEventCamera ;
-      OnDrag (e) ;
+         OnJoystickDownAction.Invoke();
+      _isTouching = true;
+      cam = e.pressEventCamera;
+      OnDrag(e);
    }
 
-   public void OnDrag (PointerEventData e) {
-      pointerEventData = e ;
+   public void OnDrag(PointerEventData e)
+   {
+      pointerEventData = e;
    }
 
-   void Update () {
-      if (_isTouching && RectTransformUtility.ScreenPointToLocalPointInRectangle (container, pointerEventData.position, cam, out point)) {
-         point = Vector2.ClampMagnitude (point, maxLength) ;
-         handle.anchoredPosition = point ;
+   void Update()
+   {
+      if (_isTouching && RectTransformUtility.ScreenPointToLocalPointInRectangle(container, pointerEventData.position, cam, out point))
+      {
+         point = Vector2.ClampMagnitude(point, maxLength);
+         handle.anchoredPosition = point;
 
-         float length = Mathf.InverseLerp (0f, maxLength, point.magnitude) ;
-         normalizedPoint = Vector2.ClampMagnitude (point, length) ;
-        }
+         float length = Mathf.InverseLerp(0f, maxLength, point.magnitude);
+         normalizedPoint = Vector2.ClampMagnitude(point, length);
+      }
 
-        ValueX.text = normalizedPoint.x.ToString();
-        ValueY.text = normalizedPoint.y.ToString();
-        OnJoystickDownEvents?.Invoke();
-        OnJoystickUpEvents?.Invoke();
-    }
+      ValueX.text = normalizedPoint.x.ToString();
+      ValueY.text = normalizedPoint.y.ToString();
+      OnJoystickDownEvents?.Invoke();
+      OnJoystickUpEvents?.Invoke();
+   }
 
-    public void OnPointerUp (PointerEventData e) {
+   public void OnPointerUp(PointerEventData e)
+   {
       if (OnJoystickUpAction != null)
          OnJoystickUpAction.Invoke();
 
-        _isTouching = false ;
-      normalizedPoint = Vector3.zero ;
-      handle.anchoredPosition = Vector3.zero ;
+      _isTouching = false;
+      normalizedPoint = Vector3.zero;
+      handle.anchoredPosition = Vector3.zero;
    }
 
-   public float Horizontal () {
+   public float Horizontal()
+   {
       if (ArrowKeysSimulationEnabled)
-         return (normalizedPoint.x != 0) ? normalizedPoint.x : Input.GetAxis ("Horizontal") ;
+         return (normalizedPoint.x != 0) ? normalizedPoint.x : Input.GetAxis("Horizontal");
 
-      return normalizedPoint.x ;
+      return normalizedPoint.x;
    }
 
-   public float Vertical () {
+   public float Vertical()
+   {
       if (ArrowKeysSimulationEnabled)
-         return (normalizedPoint.y != 0) ? normalizedPoint.y : Input.GetAxis ("Vertical") ;
+         return (normalizedPoint.y != 0) ? normalizedPoint.y : Input.GetAxis("Vertical");
 
-      return normalizedPoint.y ;
+      return normalizedPoint.y;
    }
 }
 
